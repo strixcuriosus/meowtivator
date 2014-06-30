00dcded731af78fa166e1e9f8c437c402971bda7
Ash Hoover <strixcuriosus@gmail.com>
2014-06-29 23:22:11 -0700

    Updates giphy API fetching function

--- popup.js
+++ popup.js
@@ -1,20 +1,14 @@
 
-console.log('YOYOYOYOYO');
 var searchQuery = "motivation cat";
 var urllist = [];
-var giphyUrl = 'http://api.giphy.com/v1/gifs/search?q=' +
- encodeURIComponent(searchQuery) +
-'&api_key=dc6zaTOxFJmzC';
-
-var giphyUrl3 = 'http://api.giphy.com/v1/gifs/search?q=' +
- encodeURIComponent('cat power') +
-'&api_key=dc6zaTOxFJmzC';
-
-var giphyUrl2 = 'http://api.giphy.com/v1/gifs/search?q=' +
- encodeURIComponent('lolcat inspiration') +
-'&api_key=dc6zaTOxFJmzC';
-
 var count = 0;
+
+var generateURL = function (searchQuery) {
+  var giphyUrl = 'http://api.giphy.com/v1/gifs/search?q=' +
+   encodeURIComponent(searchQuery) +
+  '&api_key=dc6zaTOxFJmzC';  //giphy's public api key (for testing purposes);
+  return giphyUrl;
+};
 
 var getGifs = function (callback, url) {
   console.log('giffin');
@@ -31,6 +25,7 @@ var getGifs = function (callback, url) {
 }
 
 var motivations = ['You can do it!', 'Meowza!', 'Hax0rCats!', 'Prrrrrfect!', 'Less QQ more mew mew'];
+var queries = ['lolcats', 'cat+tricks', 'ohai'];
 
 var setCatPic = function (url) {
   document.getElementById('catpic').src = url;
@@ -45,13 +40,14 @@ var changeCats = function () {
   console.log(next);
   setCatPic(next);
 }
-// Run our kitten generation script as soon as the document's DOM is ready.
+
 document.addEventListener('DOMContentLoaded', function () {
-  // kittenGenerator.requestKittens();
-  console.log('win loaded');
-  getGifs(setCatPic, giphyUrl2);
-  getGifs(setCatPic, giphyUrl3);
-  getGifs(setCatPic, giphyUrl);
+  queries.forEach(function(query) {
+    console.log(query);
+    var url = generateURL(query);
+    getGifs(setCatPic, url);
+  });
+
   var el = document.getElementById("more");
   el.addEventListener("click", changeCats, false);
 
